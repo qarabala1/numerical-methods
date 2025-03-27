55c1d744471930e95f3b5ef7a6143cdac9e49299
zenci125 <121859719+zenci125@users.noreply.github.com>
2025-03-28 00:57:30 +1000

    Changed input in the lab 2

--- labs_2_sem/laba2.py
+++ labs_2_sem/laba2.py
@@ -1,77 +1,72 @@
 import numpy as np
 import matplotlib.pyplot as plt
-np.set_printoptions(precision=2, suppress=True, linewidth=200, threshold=np.inf)
 
 x_1 = 0.0
 x_n = 1.0
-N = 500 
+N = 200  
 h = (x_n - x_1) / N
 x = np.linspace(x_1, x_n, N + 1)
 
-
 def p(x):
-    return -2  
+    return -(x + 1.0)**2
 
 def q(x):
-    return 1    
+    return -2.0 / (x + 1.0)**2
 
-def f(x):
-    return x**2 - 4*x + 2  
+def f_func(x):
+    return 1.0
 
 def exact_solution(x):
-    return np.exp(x) + x**2  
-
-# def compute_a_c(r):
-#     term = (2 - np.exp(r) - np.exp(-r))**2 / (2 + np.exp(r) + np.exp(-r))
-#     a_i = (2 * np.abs(r) + term - r)/  h**2
-#     c_i = (2 * np.abs(r) + term + r)/ h**2
-#     return a_i, c_i
-
-# def compute_a_c(r):
-#     a_i = (np.abs(r) + np.exp(-np.abs(r)) - r) / h**2
-#     c_i = (np.abs(r) + np.exp(-np.abs(r)) + r) / h**2
-#     return a_i, c_i
+    return 1.0 / (x + 1.0)
 
 def compute_a_c(r):
-    a_i = (1 - r + r**2 / 2) / h**2
-    c_i = (1 + r + r**2 / 2) / h**2
+    a_i = (1.0 - r) / h**2
+    c_i = (1.0 + r) / h**2
     return a_i, c_i
 
 A = np.zeros((N + 1, N + 1))
 d = np.zeros(N + 1)
 
-alpha0, beta0, gamma0 = 1, 0, 1
-A[0, 0] = alpha0
-d[0] = gamma0
+A[0, 0] = 1.0 + 3.0/(2.0*h)
+A[0, 1] = -4.0/(2.0*h)
+A[0, 2] = 1.0/(2.0*h)
+d[0] = 2.0
 
-alpha1, beta1, gamma1 = 1, 0, np.exp(1) + 1
-A[N, N] = alpha1
-d[N] = gamma1
+A[N, N] = 1.0
+d[N] = 0.5
 
 for i in range(1, N):
-    r = (p(x[i]) / 2) * h  
-    a_i, c_i = compute_a_c(r)
+    r_i = 0.5 * p(x[i]) * h
+    a_i, c_i = compute_a_c(r_i)
     
-    A[i, i-1] = a_i  
-    A[i, i] = - (a_i + c_i) + q(x[i])  
-    A[i, i+1] = c_i  
-    d[i] = f(x[i])
+    # Если i=1, исключаем u0 из уравнения
+    if i == 1:
+        coeff_u0 = A[0, 0]  
+        coeff_u1 = A[0, 1]  
+        coeff_u2 = A[0, 2] 
+
+        A[1, 1] += A[1, 0] * (-coeff_u1 / coeff_u0)
+        A[1, 2] += A[1, 0] * (-coeff_u2 / coeff_u0)
+        d[1] += A[1, 0] * (d[0] / coeff_u0)
+        A[1, 0] = 0.0  # a1 = 0
+        
+    A[i, i-1] = a_i
+    A[i, i] = - (a_i + c_i) + q(x[i])
+    A[i, i+1] = c_i
+    d[i] = f_func(x[i])
 
 u_numerical = np.linalg.solve(A, d)
 
 u_exact = exact_solution(x)
-
 error = np.max(np.abs(u_numerical - u_exact))
-print(error)
+print("Максимальная погрешность =", error)
 
-plt.figure(figsize=(12, 6))
-plt.plot(x, u_numerical, 'b-', label='Численное решение')
+plt.figure(figsize=(10, 5))
+plt.plot(x, u_numerical, 'b.-', label='Численное решение')
 plt.plot(x, u_exact, 'r--', label='Точное решение')
-plt.title('Сравнение с модифицированной схемой')
+plt.title(f'Сравнение решений, погрешность: {error:.3e}')
 plt.xlabel('x')
 plt.ylabel('u(x)')
 plt.legend()
 plt.grid(True)
 plt.show()
-
-
